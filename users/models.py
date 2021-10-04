af9489d77786b0fb688c18a83faade3594ad3975
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-10-04 14:11:47 +0400

    have some problem with migrate

--- users/models.py
+++ users/models.py
@@ -6,25 +6,29 @@ class User(AbstractUser):
 
     TYPE_CHOICES = [
         ('1', 'Cook'),
-        ('2', 'Supplier'),
-        ('3', 'Client')
+        ('2', 'Supplier')
     ]
     patronymic = models.CharField(max_length=60)
     
     # fuad / asagida unique=True -ni sildim
-    phone = models.CharField(max_length=10)
+    phone = models.CharField(max_length=10, unique=True)
     user_type = models.CharField(max_length=2, choices=TYPE_CHOICES, default='3')
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    username = None
 
     # moderations
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+    
     def __str__(self):   
         return self.first_name
 
     # fuad / asagidakilari comment-e aldim
-    # USERNAME_FIELD = 'phone'
-    # REQUIRED_FIELDS = ['first_name']
+    
 
 
 
