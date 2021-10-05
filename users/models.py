f932d2ee64fb2ef1961a3585099956b1f02815f9
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-10-05 09:23:15 +0400

    delete client choise from registr

--- users/models.py
+++ users/models.py
@@ -6,14 +6,13 @@ class User(AbstractUser):
 
     TYPE_CHOICES = [
         ('1', 'Cook'),
-        ('2', 'Supplier'),
-        ('3', 'Client')
+        ('2', 'Supplier')
     ]
     patronymic = models.CharField(max_length=60)
     
     # fuad / asagida unique=True -ni sildim
     phone = models.CharField(max_length=10)
-    user_type = models.CharField(max_length=2, choices=TYPE_CHOICES, default='3')
+    user_type = models.CharField(max_length=2, choices=TYPE_CHOICES, default='1')
 
     # moderations
     created_at = models.DateTimeField(auto_now_add=True)
