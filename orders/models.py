2d74b6fb96900b10e44095daa41941fc18454944
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-10-08 18:49:35 +0400

    add customer data

--- orders/models.py
+++ orders/models.py
@@ -2,6 +2,7 @@ from clients.models import Client
 from django.db import models
 from cooks.models import Cook
 from meals.models import Meal
+from django.core.validators import RegexValidator
 # Create your models here.
 
 
@@ -11,6 +12,9 @@ class Order(models.Model):
     cook = models.ForeignKey(Cook, db_index=True, on_delete=models.CASCADE, related_name='orders')
     # client = models.ForeignKey(Client,db_index=True, on_delete=models.CASCADE, related_name='orders' )
     #information
+
+    # customer info
+
     STATUS_CHOICES = [
         ('1', 'Order is confirmed by cook'),
         ('2', 'Preparing order'),
@@ -19,7 +23,17 @@ class Order(models.Model):
         ('5', 'Courier is on the to you'),
         ('6', 'Order is here!'),
     ]
-    status = models.CharField(max_length=2, choices=STATUS_CHOICES)
+
+    
+    
+    # about customer
+    customer_first_name = models.CharField('first name', max_length=150)
+    customer_last_name = models.CharField('last name', max_length=150)
+    customer_phone_regex = RegexValidator(regex = r"^994(?:50|51|55|70|77|99|10|60)[0-9]{7}$", message="Phone number must be entered in the format: '994709616969'. Up to 12 digits")
+    customer_phone = models.CharField(validators=[customer_phone_regex], max_length=12)
+
+    email = models.EmailField(('email address'), unique=True, max_length=254) 
+    status = models.CharField(max_length=2, choices=STATUS_CHOICES, blank=True, null=True)
     complete = models.BooleanField(default=False)
 
     # moderations
