6c5afb7867e604ae2af9e67fc81c34828d80b356
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-10-12 12:37:08 +0400

    add api for ingredients

--- meals/api/urls.py
+++ meals/api/urls.py
@@ -6,12 +6,15 @@ from . import views
 urlpatterns = [
     path("", views.mealsApiOverviews, name="api-overview"),
     path("meals/", views.MealAPIView.as_view()),
+    # path("meals/<str:pk>/", views.meal_single),
     path("meal-create/", views.meal_list),
     path("meals/<str:pk>/", views.meal_detail),
     path('categories/', views.category_list),
     path('categories/<str:pk>/', views.category_detail),
     path('mealoptions/', views.mealoption_list),
     path('mealoptions/<str:pk>/', views.mealoption_detail),
+    path('ingredients/', views.ingredient_list),
+    path('ingredients/<str:pk>/', views.ingredient_detail),
     # path(r'^api/tutorials/published$', views.tutorial_list_published)
 ]
 
