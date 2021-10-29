005d2183177959b02aa9d801ed1f1afec22e0dbd
fuad_suleymanov <fuadsuleymanov@outlook.com>
2021-10-29 16:41:33 +0400

    cook update work

--- cooks/api/urls.py
+++ cooks/api/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from cooks.api.views import (CookActiveOrdersAPIView, CookMealsAPIView, CookOrdersAPIView, CookRecommendationsAPIView, CooksAPIView, 
-                             CookResumesAPIView, RecommendationsAPIView, ResumesAPIView, cook_detail, getCookByUsername, getCookByUsernameBool)
+                             CookResumesAPIView, RecommendationsAPIView, ResumesAPIView, cook_detail, cookCreateUpdate, getCookByUsername, getCookByUsernameBool)
 
 app_name = 'cooks_api'
 
 urlpatterns = [
     path('cooks/', CooksAPIView.as_view(), name='cooks'),
     path('cooks/<int:pk>/', cook_detail, name='cook'),
+    path('cook-create/<str:username>', cookCreateUpdate, name='cook-create'),
     path('cooks/<str:username>/', getCookByUsername, name='cookByUsername'),
     path('cook-check/<str:username>/', getCookByUsernameBool, name='checkUser'),
     path('recommendations/', RecommendationsAPIView.as_view(), name='recommendations'),
@@ -16,5 +17,4 @@ urlpatterns = [
     path('cooks/<int:pk>/meals/', CookMealsAPIView.as_view(), name='cook-meals'),
     path('cooks/<int:pk>/orders/', CookOrdersAPIView.as_view(), name='cook-orders'),
     path('cooks/<int:pk>/activeorders/', CookActiveOrdersAPIView.as_view(), name='cook-activeorders'),
-
 ]
