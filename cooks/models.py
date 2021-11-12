f668e918344d454b5676feb8ec6c937c9f1c84c3
fuadsuleyman <fuadsuleymanov@outlook.com>
2021-11-12 15:34:41 +0400

    correct clients model again

--- cooks/models.py
+++ cooks/models.py
@@ -7,7 +7,7 @@ class Client(models.Model):
     last_name = models.CharField(max_length=50)
     patronymic = models.CharField(max_length=70, null=True)
     username = models.CharField(max_length=50)
-    user_type = models.CharField(max_length=4)
+    user_type = models.CharField(max_length=4, null=True)
     phone_number = models.CharField(max_length=20, null=True)
     email = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
