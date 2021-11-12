bd8be89120885ae313e4a4cfa3a206e77c085000
fuadsuleyman <fuadsuleymanov@outlook.com>
2021-11-12 16:43:30 +0400

    ageni add with get

--- cooks/api/views.py
+++ cooks/api/views.py
@@ -57,7 +57,7 @@ test_param = openapi.Parameter('test', openapi.IN_QUERY, description="test manua
 def cookCreate(request):
     print("****************")
     print("cook create-e daxil oldu")
-    client1 = Client.objects.filter(id = 1).first()
+    client1 = Client.objects.get(id = 1)
     # print(client1, "cleint 1")
     # try: 
     #     client1 = Client.objects.get(id = 1) 
