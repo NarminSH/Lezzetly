01c6c76295bdaed488845390811c103bc49f8734
fuadsuleyman <fuadsuleymanov@outlook.com>
2021-11-15 14:13:46 +0400

    add location field to client in python

--- cooks/models.py
+++ cooks/models.py
@@ -9,6 +9,7 @@ class Client(models.Model):
     username = models.CharField(max_length=50)
     user_type = models.CharField(max_length=4, null=True)
     phone_number = models.CharField(max_length=20, null=True)
+    location = models.CharField(max_length=250, null=True)
     email = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
