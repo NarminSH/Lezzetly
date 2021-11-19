8f71886c569f3549f3f580e79aee24ff3a226256
Narmin <narmin@Narmins-MacBook-Air.local>
2021-11-19 15:54:58 +0400

    remove client from admin panel

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from delivery.models import (
-    Courier,
+    # Courier,
     DeliveryArea,
     DeliveryPrice
 )
 
 # Register your models here.
-@admin.register(Courier)
-class CourierAdmin(admin.ModelAdmin):
-    list_display = ('id', 'first_name','last_name','transport','rating','work_experience','is_available','created_at')
-    list_filter = ('id', 'first_name','rating','transport','created_at')
-    search_fields = ('id','first_name', 'transport')
+# @admin.register(Courier)
+# class CourierAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'first_name','last_name','transport','rating','work_experience','is_available','created_at')
+#     list_filter = ('id', 'first_name','rating','transport','created_at')
+#     search_fields = ('id','first_name', 'transport')
 
 @admin.register(DeliveryArea)
 class DeliveryAreaAdmin(admin.ModelAdmin):
