05b402bba8c3ffcc4ca808e2469352c068493491
fuadsuleyman <fuadsuleymanov@outlook.com>
2021-11-20 17:33:05 +0400

    try get order item data3

--- orders/api/views.py
+++ orders/api/views.py
@@ -55,7 +55,7 @@ def order_create(request):
     # create empty order with out orderItems, with customer data and add cook
     
     order_item_data = order_data['order_items']
-    logger.info("just check logger")
+    # logger.info("just check logger")
     print("order_item_data: ", order_item_data)
     return JsonResponse({'message': f'biraz da derine, order_item_data: {order_item_data}'}, status=status.HTTP_200_OK)
     if not order_item_data:
