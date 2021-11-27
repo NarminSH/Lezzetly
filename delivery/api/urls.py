ee01f96fa26f521cead47697df377a721c90567b
fuadsuleyman <fuadsuleymanov@outlook.com>
2021-11-27 19:37:00 +0400

    change urls in delivery

--- delivery/api/urls.py
+++ delivery/api/urls.py
@@ -10,10 +10,10 @@ urlpatterns = [
     path('couriers/<int:pk>/', courier_detail, name='courier'),
     # path('couriers/<int:pk>/', CourierAPIView.as_view(), name='courier'),
     path('couriers/<int:pk>/orders/', CourierOrdersAPIView.as_view(), name='courierorders'),
-    path('couriers/<int:pk>/activeorders/', CourierActiveOrdersAPIView.as_view(), name='courier-activeorders'),
-    path('deliveryareas/', DeliveryAreasAPIView.as_view(), name='deliveryareas'),
-    path('couriers/<int:pk>/deliveryareas/', CourierAreasAPIView.as_view(), name='courierareas'),
-    path('couriers/<int:pk>/deliveryareas/<int:id>', CourierAreaAPIView.as_view(), name='courierarea'),
-    path('couriers/deliveryareas/', CouriersDeliveryAreasAPIView.as_view(), name='allcouriersareas'),
-    path('couriers/deliveryareas/<int:area_id>', DeliveryAreaCouriersAPIView.as_view(), name='deliveryarea'),
+    path('couriers/<int:pk>/activeorders', CourierActiveOrdersAPIView.as_view(), name='courier-activeorders'),
+    path('deliveryareas', DeliveryAreasAPIView.as_view(), name='deliveryareas'), # all areas
+    path('deliveryareas/couriers/<int:pk>', CourierAreasAPIView.as_view(), name='courierareas'), # create and all areas of courier
+    path('deliveryareas/<int:pk>/couriers/<int:id>', CourierAreaAPIView.as_view(), name='courierarea'),
+    path('deliveryareas/couriers', CouriersDeliveryAreasAPIView.as_view(), name='allcouriersareas'),
+    path('deliveryareas/<int:area_id>', DeliveryAreaCouriersAPIView.as_view(), name='deliveryarea'),
 ]
